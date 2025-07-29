ca2c62488784c1b4c0109f699c85ead82a844b3d
PHET45 <phnjk2000@gmail.com>
2025-07-29 14:52:04 +0700

    feat: implement updateCarCollection function to manage car brands in the collection

--- ex-6.js
+++ ex-6.js
@@ -1,9 +1,14 @@
 const carCollection = ["toyota", "fiat", "honda", "bmw"];
 
 function updateCarCollection(carBrand) {
-  // Start coding here
-}
-
+  if(carCollection.includes(carBrand)){
+    let  position = carCollection.indexOf(carBrand)
+    return `${carBrand} has already existed in the ${position+1} position of car collection.`
+  } else if(!carCollection.includes(carBrand)){
+     carCollection.push(carBrand)
+    return `new car collection is ${carCollection}: `
+  }
+};
 //ผลลัพธ์ที่ควรได้จาก Example case
 //ในกรณีที่ยังไม่มียี่ห้อรถใน carCollection
 console.log(updateCarCollection("audi")); //new car collection is : toyota,fiat,honda,bmw,audi.
